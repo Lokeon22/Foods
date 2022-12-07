4bdd4e16680734b5b116fcb69be728c9ac3ae64e
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2022-12-07 20:48:19 -0300

    section plates updated

--- src/components/Plates.jsx
+++ src/components/Plates.jsx
@@ -1,63 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import heart from "../assets/icons/HeartStraight.svg";
-import prato1 from "../assets/foods/prato1.png";
 import minus from "../assets/icons/Minus.svg";
 import plus from "../assets/icons/Plus.svg";
+import heart from "../assets/icons/HeartStraight.svg";
+import redheart from "../assets/icons/redheart.svg";
+
+import pratos from "../data";
 
 export const Plates = () => {
   const [amount, setAmount] = useState(1);
-  const [price, setPrice] = useState(25.97);
+  const [coracao, setCoracao] = useState(false);
 
-  const changeminusPrice = () => {
-    if (amount <= 1) {
-      return;
-    }
-    setAmount(amount - 1);
-    setPrice((prevValue) => prevValue - 25.97);
-  };
-
-  const changeplusPrice = () => {
-    if (amount >= 10) {
-      return alert("Quantidade de pratos atingido!");
-    }
-    setAmount(amount + 1);
-    setPrice((prevValue) => prevValue + 25.97);
+  const teste = () => {
+    setCoracao(!coracao);
   };
 
   return (
-    <div className="max-w-[300px] px-10 py-14 mb-[400px]">
-      <img src={heart} className="w-8 h-8 cursor-pointer my-0 mx-[90%]" />
-      <div className="flex flex-col items-center justify-center">
-        <img src={prato1} className="w-44 h-44" />
-        <h2 className="text-[22px] font-Poppins font-semibold mt-4 mb-4">
-          Torradas de Parma
-        </h2>
-        <p className="text-xs font-Roboto font-normal text-center">
-          Presunto de parma e rúcula em um pão com fermentação natural.
-        </p>
-        <span className="text-[#82F3FF] text-3xl font-Roboto mt-4 mb-4">
-          R$ {price}
-        </span>
-        <form className="flex flex-wrap justify-center items-center gap-4">
-          <button type="button" onClick={changeminusPrice}>
-            <img src={minus} className="w-6 h-6" />
-          </button>
-          <p className="font-Roboto font-semibold text-lg">{amount}</p>
-          <button type="button" onClick={changeplusPrice}>
-            <img src={plus} className="w-6 h-6" />
-          </button>
-          <button
-            type="submit"
-            className="bg-[#92000E] px-4 py-2 rounded font-Poppins"
-            onClick={(event) => {
-              event.preventDefault();
-            }}
-          >
-            incluir
-          </button>
-        </form>
-      </div>
+    <div className="flex flex-col items-center justify-center md:flex-row gap-7">
+      {pratos.map(({ id, title, desc, preco, image }) => (
+        <div
+          key={id}
+          className="max-w-[300px] flex py-14 px-10 flex-col items-center"
+        >
+          {coracao ? (
+            <img
+              src={redheart}
+              className="w-8 h-8 cursor-pointer self-end"
+              onClick={teste}
+            />
+          ) : (
+            <img
+              src={heart}
+              className="w-8 h-8 cursor-pointer self-end"
+              onClick={teste}
+            />
+          )}
+          <img src={image} className="w-44 h-44" />
+          <h2 className="text-[22px] text-center font-Poppins font-semibold mt-4 mb-4">
+            {title}
+          </h2>
+          <p className="text-xs font-Roboto font-normal text-center">{desc}</p>
+          <span className="text-[#82F3FF] text-3xl font-Roboto mt-4 mb-4">
+            R$ {preco}
+          </span>
+          <form className="flex flex-wrap justify-center items-center gap-4">
+            <button type="button">
+              <img src={minus} className="w-6 h-6" />
+            </button>
+            <p className="font-Roboto font-semibold text-lg">{amount}</p>
+            <button type="button">
+              <img src={plus} className="w-6 h-6" />
+            </button>
+            <button
+              type="submit"
+              className="bg-[#92000E] px-4 py-2 rounded font-Poppins"
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              incluir
+            </button>
+          </form>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/data/index.jsx
@@ -0,0 +1,29 @@
+import prato1 from "../assets/foods/prato1.png";
+import prato2 from "../assets/foods/prato2.png";
+import prato3 from "../assets/foods/prato3.png";
+
+const pratos = [
+  {
+    id: 1,
+    title: "Torradas de Parma",
+    desc: "Presunto de parma e rúcula em um pão com fermentação natural.",
+    preco: "25,97",
+    image: prato1,
+  },
+  {
+    id: 2,
+    title: "Spaguetti Gambe",
+    desc: "Massa fresca com camarões e pesto recheado.",
+    preco: "79,97",
+    image: prato2,
+  },
+  {
+    id: 3,
+    title: "Salada Molla",
+    desc: "Tomates, coentro, pepino, cebola roxa. Frescos e temperados.",
+    preco: "19,97",
+    image: prato3,
+  },
+];
+
+export default pratos;
